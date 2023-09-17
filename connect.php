ac966cb52e96a2f942cc5dd8036f3e8d7f2f7c63
Thorstein <tlfougner@gmail.com>
2023-09-17 14:57:58 +0200

    Initial commit

---
+++ connect.php
@@ -0,0 +1,8 @@
+<?php
+# This information is censored for security reasons
+$serverName = "localhost";
+$dBUsername = " XXX";
+$dBPassword = "XXX";
+$dBname = "XXX";
+
+$conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBname);
